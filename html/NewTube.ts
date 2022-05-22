d720979dceb8b5184615cab9da9b370ac169924f
stealpotsyear <anny919214@gmail.com>
2022-05-22 20:04:52 +0900

    second

---
+++ html/NewTube.ts
@@ -0,0 +1,14 @@
+// person.ts
+class NewTube extends HTMLElement{
+    source: string;
+  
+    constructor(source: string) {
+        super();
+        this.source = source;
+    }
+
+    generate(source){
+        
+    }
+  }
+  
